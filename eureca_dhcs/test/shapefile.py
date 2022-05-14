592be2ef60fba9de69f3d0ed80923ca5d52c765d
Prataviera Enrico <enrico.prataviera@studenti.unipd.it>
2022-05-14 10:36:52 -0700

    Check of some simulations for pressure losses

--- eureca_dhcs/test/shapefile.py
+++ eureca_dhcs/test/shapefile.py
@@ -26,3 +26,7 @@ path_nodes = os.path.join("eureca_dhcs", "test", "input_tests", "nodes.shp")
 network = Network.from_shapefiles(path_nodes, path_lines)
 # %%
 x = network.solve_hydraulic_balance()
+for i, n in enumerate(network._nodes_object_ordered_list):
+    print(i, n._idx, x[network._branches_number + i] / 1000)
+for i, b in enumerate(network._branches_object_ordered_list):
+    print(i, b._idx, x[i], b._pipe_len, b._pipe_diameter)
