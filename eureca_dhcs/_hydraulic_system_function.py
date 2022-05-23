c445a3ad8974bbab566c7b97ffe299910dc9775b
Prataviera Enrico <enrico.prataviera@studenti.unipd.it>
2022-05-23 14:00:36 -0700

    if for reinold in hydraulic system

--- eureca_dhcs/_hydraulic_system_function.py
+++ eureca_dhcs/_hydraulic_system_function.py
@@ -96,55 +96,59 @@ def hydraulic_balance_system(x, q, network):
         #         + network._nodes_numbes
         #         + branch._unique_matrix_idx
         #     ]
-        # reinolds = (
-        #     4
-        #     * x[branch._unique_matrix_idx]
-        #     / (np.pi * branch.get_dynamic_viscosity() * branch._pipe_int_diameter)
-        # )
-        # if reinolds > 2300:
-        system.append(
-            1
-            / np.sqrt(
+        reinolds = (
+            4
+            * x[branch._unique_matrix_idx]
+            / (np.pi * branch.get_dynamic_viscosity() * branch._pipe_int_diameter)
+        )
+        if np.abs(reinolds) > 2300:
+            system.append(
+                1
+                / np.sqrt(
+                    x[
+                        network._branches_number
+                        + network._nodes_number
+                        + branch._unique_matrix_idx
+                    ]
+                )
+                + 2
+                * np.log(
+                    branch._roughness / (3.7 * branch._pipe_int_diameter)
+                    + np.pi
+                    * 2.51
+                    * branch._pipe_int_diameter
+                    * branch.get_dynamic_viscosity()
+                    / (
+                        4
+                        * np.abs(x[branch._unique_matrix_idx])
+                        * np.sqrt(
+                            x[
+                                network._branches_number
+                                + network._nodes_number
+                                + branch._unique_matrix_idx
+                            ]
+                        )
+                    )
+                )
+            )
+        else:
+            system.append(
                 x[
                     network._branches_number
                     + network._nodes_number
                     + branch._unique_matrix_idx
                 ]
-            )
-            + 2
-            * np.log(
-                branch._roughness / (3.7 * branch._pipe_int_diameter)
-                + np.pi
-                * 2.51
-                * branch._pipe_int_diameter
-                * branch.get_dynamic_viscosity()
+                - 64
                 / (
                     4
-                    * np.abs(x[branch._unique_matrix_idx])
-                    * np.sqrt(
-                        x[
-                            network._branches_number
-                            + network._nodes_number
-                            + branch._unique_matrix_idx
-                        ]
+                    * x[branch._unique_matrix_idx]
+                    / (
+                        np.pi
+                        * branch.get_dynamic_viscosity()
+                        * branch._pipe_int_diameter
                     )
                 )
             )
-        )
-        # else:
-        #     system.append(
-        #         x[
-        #             network._branches_number
-        #             + network._nodes_number
-        #             + branch._unique_matrix_idx
-        #         ]
-        #         - 64
-        #         / (
-        #             4
-        #             * x[branch._unique_matrix_idx]
-        #             / (np.pi * branch.get_dynamic_viscosity() * branch._pipe_int_diameter)
-        #         )
-        #     )
     system.append(
         x[network._branches_number + network._nodes_number - 1]
         - q[network._branches_number + network._nodes_number - 1]
